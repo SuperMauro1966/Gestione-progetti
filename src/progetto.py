c2dc7ac93e521ba1a7c73613d64bcf929f5c8ae3
kevin <shahini.kevin@studenti.alberghetti.it>
2025-05-29 12:12:39 +0200

    aggiunta di un menù

--- src/progetto.py
+++ src/progetto.py
@@ -1,42 +1,51 @@
-import mariadb
+from db import conn
 
-conn_params= {
-    "user" : "root",
-    "password" : "12345678",
-    "host" : "localhost",
-    "database" : "gestione_progetti"
-}
- 
-connection= mariadb.connect(**conn_params)
+__all__ = ["crea_progetto"]
 
-cursor = connection.cursor(dictionary=True)
+def menu():
+    print("1. crea progetto")
+    print("2. cancella progetto")
+    print("3. ritorno al menù principale")
 
-print("inserire i dati per la creazione del progetto")
-while True:
-    print("nome: ")
-    nome=input("-->")
 
-    cursor.execute("SELECT COUNT(*) FROM progetto WHERE Nome = ?", (nome,))
+def dlg_crea_progetto():
+    print("inserire i dati per la creazione del progetto")
+    presente = True
+    while presente:
+        print("nome: ")
+        nome=input("-->")
+
+        presente = is_progetto_present_by_name(conn, nome)
+
+        if presente :
+            print("inserisci un nome diverso")
+
+    print("descrizione: ")
+    descrizione=input("-->")
+    print("data inizio:")
+    data_inizio=input("-->")
+    print("data fine: ")
+    data_fine=input("-->")
+
+    crea_progetto(conn,  nome, descrizione, data_inizio, data_fine)
+
+def crea_progetto(conn, nome, desc, di, df):
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("INSERT INTO progetto(Nome, Descrizione, Data_Inizio, Data_Fine ) VALUES (?,?,?,?)", (nome, desc, di, df))
+    
+    conn.commit()
+
+def is_progetto_present_by_name(conn, nome):
+    cursor = conn.cursor(dictionary=True)
+
+    cursor.execute("SELECT COUNT(*) AS nr FROM progetto WHERE Nome = ?", (nome,))
 
     result = cursor.fetchone()
-    count = result['COUNT(*)']
+    count = result['nr']
 
-    if count > 0:
-        print("Nome già esistente. Inserisci un nome diverso.")
-    elif nome == "":
-        print("Il nome non può essere vuoto.")
-    else:
-        break
+    return count > 0
 
-print("descrizione: ")
-descrizione=input("-->")
-print("data inizio:")
-data_inizio=input("-->")
-print("data fine: ")
-data_fine=input("-->")
 
-cursor.execute("INSERT INTO progetto(Nome, Descrizione, Data_Inizio, Data_Fine ) VALUES (?,?,?,?)", (nome, descrizione, data_inizio, data_fine))
- 
-connection.commit()
 
-connection.close()
+
