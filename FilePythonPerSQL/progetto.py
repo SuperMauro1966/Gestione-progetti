48752123389c5979799c7e358b87a5bd16e30426
kevin <shahini.kevin@studenti.alberghetti.it>
2025-05-29 09:11:46 +0200

    controllo nome

--- FilePythonPerSQL/progetto.py
+++ FilePythonPerSQL/progetto.py
@@ -12,10 +12,21 @@ connection= mariadb.connect(**conn_params)
 cursor = connection.cursor(dictionary=True)
 
 print("inserire i dati per la creazione del progetto")
+while True:
+    print("nome: ")
+    nome=input("-->")
 
-print("nome: ")
-nome=input("-->")
+    cursor.execute("SELECT COUNT(*) FROM progetto WHERE Nome = ?", (nome,))
 
+    result = cursor.fetchone()
+    count = result['COUNT(*)']
+
+    if count > 0:
+        print("Nome già esistente. Inserisci un nome diverso.")
+    elif nome == "":
+        print("Il nome non può essere vuoto.")
+    else:
+        break
 
 print("descrizione: ")
 descrizione=input("-->")
