9c017db3b30f24d09b46ca7f7ac3f81d26707a9f
kevin <shahini.kevin@studenti.alberghetti.it>
2025-06-03 09:25:20 +0200

    aggiunta sezione WBS

--- src/main.py
+++ src/main.py
@@ -1,10 +1,14 @@
+import user_interface.WBS
 import user_interface.progetto
 
 # menù principale
 
 print("1. progetti")
+print("2. WBS")
 
 scelta=input(" scelta --> ")
 
 if scelta == '1':
     user_interface.progetto.menu()
+elif scelta == '2':
+    user_interface.WBS.menu_wbs()
