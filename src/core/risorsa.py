9b16c31a138ccfc7d26926207e99214ec09e573b
wassim <faqui.wassim@studenti.alberghetti.it>
2025-06-04 09:17:40 +0200

    aggiustati vari errori di chiamata

--- src/core/risorsa.py
+++ src/core/risorsa.py
@@ -30,4 +30,11 @@ def modifica_risorsa(risorsa_id, quantita, unita, descrizione):
     cursor = conn.cursor(dictionary=True)
     cursor.execute(
         "UPDATE risorsa SET Quantita = ?, UnitaDiMisura = ?, Descrizione_Risorsa = ? WHERE ID = ?",
-        (quantita, unita, descr
+        (quantita, unita, descrizione, risorsa_id)
+    )
+    conn.commit()
+
+def cancella_risorsa(risorsa_id):
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("DELETE FROM risorsa WHERE ID = ?", (risorsa_id,))
+    conn.commit()
--- src/user_interface/risorsa.py
+++ src/user_interface/risorsa.py
@@ -1,11 +1,10 @@
-from core.risorsa import 
-                        crea_risorsa,\
-                        get_all_risorse,\
-                        get_risorsa_by_id,\
-                        modifica_risorsa,\
-                        cancella_risorsa
-
-
+from core.risorsa import (
+    crea_risorsa,
+    get_all_risorse,
+    get_risorsa_by_id,
+    modifica_risorsa,
+    cancella_risorsa
+)
 
 __all__ = ["menu_risorse"]
 
