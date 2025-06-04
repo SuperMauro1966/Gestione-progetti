25520a328cecec4c4dcbd88f6e3be78e71cddd03
wassim <faqui.wassim@studenti.alberghetti.it>
2025-06-04 08:59:48 +0200

    aggiunta risorse a main

---
+++ src/user_interface/main.py
@@ -0,0 +1,26 @@
+import user_interface.WBS
+import user_interface.progetto
+import user_interface.risorsa
+
+
+
+while True:
+    print("\n--- Menu Principale ---")
+    print("1. Progetti")
+    print("2. WBS")
+    print("3. Risorse")
+    print("4. Esci")
+
+    scelta = input("Scelta --> ")
+
+    if scelta == '1':
+        user_interface.progetto.menu()
+    elif scelta == '2':
+        user_interface.WBS.menu_wbs()
+    elif scelta == '3':
+        user_interface.risorsa.menu_risorse()
+    elif scelta == '4':
+        print("Uscita dal programma.")
+        break
+    else:
+        print("Scelta non valida, riprova.")
