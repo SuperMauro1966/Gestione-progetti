9782261870af27b6b8a272111e9b07488c727c8e
kevin <shahini.kevin@studenti.alberghetti.it>
2025-06-04 09:28:51 +0200

    sistemata la funzione menu

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 import user_interface.WBS
 import user_interface.progetto
+import user_interface.task
 
 # menù principale
 
 print("1. progetti")
 print("2. WBS")
+print("3. task")
 
 scelta=input(" scelta --> ")
 
@@ -12,3 +14,5 @@ if scelta == '1':
     user_interface.progetto.menu()
 elif scelta == '2':
     user_interface.WBS.menu_wbs()
+elif scelta == '3':
+    user_interface.task.menu_task()
