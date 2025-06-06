c80fba4656940701ff8c9ea16546d6b200ac7354
wassim <faqui.wassim@studenti.alberghetti.it>
2025-06-06 12:08:55 +0200

    aggiunta funzione per uscire con ctrl_c

--- src/main.py
+++ src/main.py
@@ -1,42 +1,84 @@
 import user_interface.WBS
-import user_interface.progetto
-import user_interface.task
 import user_interface.risorsa
+import user_interface.task
 import user_interface.milestone
 import user_interface.work_package
+import user_interface.progetto
 
 def main():
-    while True:
-        print("\n--- Menu Principale ---")
-        print("1. Progetti")
-        print("2. WBS")
-        print("3. Risorse")
-        print("4. task")
-        print("5. milestone")
-        print("6. work package")
-        print("7. Esci")
+    try:
+        while True:
+            print("\n--- Menu Principale ---")
+            print("1. Progetti")
+            print("2. WBS")
+            print("3. Risorse")
+            print("4. Task")
+            print("5. Milestone")
+            print("6. Work Package")
+            print("Premi CTRL-C per uscire")
+
+            scelta = input("Scelta --> ")
+
+            match scelta:
+                case '1':
+                    user_interface.progetto.menu()
+                case '2':
+                    user_interface.WBS.menu_wbs()
+                case '3':
+                    user_interface.risorsa.menu_risorse()
+                case '4':
+                    user_interface.task.menu_task()
+                case '5':
+                    user_interface.milestone.menu_milestone()
+                case '6':
+                    user_interface.work_package.menu_wp()
+                case _:
+                    print("Scelta non valida, riprova.")
+    except KeyboardInterrupt:
+        print("\nUscita dal programma...")
+        return
 
 
-        scelta = input("Scelta --> ")
 
-
-        if scelta == '1':
-            user_interface.progetto.menu()
-        elif scelta == '2':
-            user_interface.WBS.menu_wbs()
-        elif scelta == '3':
-            user_interface.risorsa.menu_risorse()
-        elif scelta == '4': 
-            user_interface.task.menu_task()
-        elif scelta == '5': 
-            user_interface.milestone.menu_milestone()
-        elif scelta == '6':
-            user_interface.work_package.menu_wp()
-        elif scelta == '7':
-            break
-        else:
-            print("Scelta non valida, riprova.")
 
 if __name__ == "__main__":
     main()
+
+"""def main():
+    while True:
+            print("\n--- Menu Principale ---")
+            print("1. Progetti")
+            print("2. WBS")
+            print("3. Risorse")
+            print("4. Task")
+            print("5. Milestone")
+            print("6. Work Package")
+            print("Premi CTRL-C per uscire")
+
+            scelta = input("Scelta --> ")
+
+            match scelta:
+                case '1':
+                    user_interface.progetto.menu()
+                case '2':
+                    user_interface.WBS.menu_wbs()
+                case '3':
+                    user_interface.risorsa.menu_risorse()
+                case '4':
+                    user_interface.task.menu_task()
+                case '5':
+                    user_interface.milestone.menu_milestone()
+                case '6':
+                    user_interface.work_package.menu_wp()
+                case _:
+                    print("Scelta non valida, riprova.")
+
+
+
+if __name__ == '__main__':
+    try:
+        main()
+    except KeyboardInterrupt:
+        print ("Esecuzione Interrotta")"""
+
 
